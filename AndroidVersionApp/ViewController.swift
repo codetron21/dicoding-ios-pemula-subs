2a0230870b5f9cf8e996d09b9fb06453c5b630e7
codetron <codetronclan@gmail.com>
2023-01-07 21:08:17 +0700

    Initial Commit

---
+++ AndroidVersionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AndroidVersionApp
+//
+//  Created by Apple on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
